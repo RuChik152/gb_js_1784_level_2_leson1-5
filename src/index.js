5d6fd601c302690c3bf3229e50e5e2473f594905
Nikita Chernov <chernovn152@gmail.com>
2022-01-20 21:01:30 +0300

    main

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./css/style.css";
+import { getProductList, getMenuList } from './mock/data.js'
+import { renderList, renderMenuLIst } from './modul.js'
+import "./css/style.scss"
+
+
+const productList = getProductList(10);
+const menulist = getMenuList();
+
+renderList(productList);
+renderMenuLIst(menulist);
